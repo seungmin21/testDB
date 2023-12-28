d04f6f61c5a0030a62ea971bdc300431fa34aab5
ohseungmin <ohmin2886@gmail.com>
2023-12-28 17:31:58 +0900

    데이터 읽기 및 삽입

--- app.js
+++ app.js
@@ -33,3 +33,28 @@ connection.query('CREATE TABLE classes (id INT AUTO_INCREMENT, room_number VARCH
   }
 });
 
+// 테이블 생성
+//connection.query('CREATE TABLE classes (id INT AUTO_INCREMENT, room_number VARCHAR(255), instructor VARCHAR(255), field VARCHAR(255), computers INT, students JSON, PRIMARY KEY (id))', (err, result) => {
+//  if (err) {
+//    console.log(err);
+//  } else {
+//    console.log('테이블 생성 성공');
+//  }
+//});
+
+// 데이터 읽기
+const data = fs.readFileSync('./test.json', 'utf-8');
+
+// 데이터 삽입
+const items = JSON.parse(data);
+items.forEach((item) => {
+  connection.query('INSERT INTO classes (room_number, instructor, field, computers, students) VALUES (?, ?, ?, ?, ?)', [item.room_301.room_number, item.room_301.instructor, item.room_301.field, item.room_301.computers, JSON.stringify(item.room_301.students)], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('데이터 삽입 성공');
+    }
+  });
+});
+
+app.listen(3000);
